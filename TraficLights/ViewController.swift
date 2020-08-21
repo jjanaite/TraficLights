864504d8058f4d0d85130293247ba01860c87912
Justine <justine.janaite@gmail.com>
2020-08-21 18:39:36 +0300

    Initial Commit

---
+++ TraficLights/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TraficLights
+//
+//  Created by justine.janaite on 21/08/2020.
+//  Copyright © 2020 justine.janaite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
